29cf21d688411edcdaa79dc59fc5a9aa357c1077
nokonoko1996 <noborikawa.ko1996@gmail.com>
2024-12-29 10:15:01 +0900

    Add files via upload

---
+++ 2 - htmlCssDesign/pracJavaScript/hello.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log(1 + 2);
+
+let price = 100;
+console.log(price);
+
+let name = "Jon";
+console.log(name);
---
+++ 2 - htmlCssDesign/pracJavaScript/typeof.js
@@ -0,0 +1,18 @@
+console.log(typeof 10);
+console.log(5 < 3);
+console.log(typeof null);
+
+console.log(1 == 1); // true
+console.log(1 != 1); // false
+console.log("1" === "1"); // true
+console.log("1" !== "1"); // false
+
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false
+
+const longText = "こんにちは\n" +
+  "こんばんは\n" +
+  "おはよう";
+
+  console.log(longText);
+  
